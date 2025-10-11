530fb08fa7e833fb41e02bbb3e1218540da42a9f
adarsh745 <adarshjanjirala745@gmail.in>
2025-10-11 16:54:41 +0530

    store

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -4,17 +4,44 @@ import store from '../models/store.model.js';
 import ApiResponse from '../utils/ApiResponse.js';
 import TryCatch from '../utils/TryCatch.js';
 
-class storeControllers {
-// getstoreById=TryCatch(async(req,res)=>{
-//         const storeData=await store.findById( req.params.id)
-//     if (!storeData)throw new ApiError("store id is not found",404,false)
-//             res.json( new ApiResponse("Data Fetched successfully",200 ,storeData))
+
+class storeControllers{
+
+        CreateStore=TryCatch(async (req , res)=>{
+        console.log(req.body)
+
+        const parse = storevalidation.safeParse(req.body)
+                if(!parse.success) throw new ApiError("validation failed" , 401 , false)
+                    
+        console.log("this is location of the store" , parse)
+        
+        const{id}=req.params
+        const formdata = {...parse.data , clusterId:id}
+        console.log("cluster Id",id)
+        console.log("form data ",formdata)
 
 
-//     }
+        const newStore = await store.create(formdata)
+
+        res.json(new ApiResponse("Store created successfully" , 201 ,newStore ))
+        
+    })
 
 
-    getStores = TryCatch(async (req, res) => {
+    getstoreById=TryCatch(async(req,res)=>
+        {
+        const storeData=await store.findById( req.params.id)
+         if (!storeData)
+            {
+                throw new ApiError("store id is not found",404,false)
+            }
+
+        res.json( new ApiResponse("Data Fetched successfully",200 ,storeData))
+
+
+    })
+
+        getStores = TryCatch(async (req, res) => {
         const storeData = await store.find({})
         res.json(new ApiResponse("Data Fetched successfully", 200, storeData))
 
@@ -23,3 +50,5 @@ class storeControllers {
 
 
 }
+
+export default storeControllers;
--- src/schemas/store.schema.ts
+++ src/schemas/store.schema.ts
@@ -1,15 +1,12 @@
 import *  as z from 'zod'
 
 export const storevalidation =z.object({
-    name:z.string(),
-    // clusterID:z.string().optional(),
-    // inventry:z.string().optional(),
     storeType:z.enum(['mother_store','mini_store']),
     storeName:z.string(),
-    locations:z.array(z.object({
+    locations:z.object({
             lat:z.number(),
             lng:z.number()
-        }))
+        })
     })
 
 
